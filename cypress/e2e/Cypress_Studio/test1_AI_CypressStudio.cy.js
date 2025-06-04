c18e2bbd194956f75a0074fab8c63a97395a030a
Asif Baba Mohammad <asifbaba.mohammad@infor.com>
2025-06-04 13:48:53 +0530

    fixed error

--- cypress/e2e/Cypress_Studio/test1_AI_CypressStudio.cy.js
+++ cypress/e2e/Cypress_Studio/test1_AI_CypressStudio.cy.js
@@ -16,7 +16,7 @@ describe('Cypress Studio Demo', () => {
         //below code belongs to Checkout page
         // Fix for the assertion error - using contains instead of have.text
         cy.get('.prodTotal > p').should('contain', '$ 31500');
-        cy.get('.removeWrap > .btn-primary').click();
+        cy.get('.removeWrap > .btn-primary').first().click();
         cy.get('.form-group > .input').clear('i');
         cy.get('.form-group > .input').type('india');
         cy.get('.ta-results > :nth-child(3)').click();
